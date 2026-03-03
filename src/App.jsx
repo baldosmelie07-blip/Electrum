3982d0bc6fb6dfa65e67f79e85b21668709e85c0
baldosmelie07 <baldosmelie07@gmail.com>
2026-03-03 08:03:34 +0800

    Added client project files

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+function App() {
+  return (
+    <div className="text-center mt-20">
+      <h1 className="text-3xl font-bold">Electrum Client</h1>
+      <p>React + Vite + Tailwind is working!</p>
+    </div>
+  );
+}
+
+export default App;
